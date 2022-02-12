74e240e3eb688418647c2339ca9ef56cb3a11c53
Ireshani99 <ireshanikollure99@gmail.com>
2022-02-12 08:27:43 +0000

    online  patient add

---
+++ src/components/OnlinePatientAdd.jsx
@@ -0,0 +1,131 @@
+import React from "react";
+import '../components/OnlinePatientAdd.css';
+import HeaderDoctorProfile from "./HeaderDoctorProfile";
+import AfterloginFutter from "./AfterloginFutter";
+
+
+function OnlinePatientAdd(){
+
+    return(
+      <>
+      <div className="form-body">
+      <HeaderDoctorProfile/>
+      
+
+      <div className="form" >
+      <div className="container">
+      <form className="was-validated">
+      <div>
+          <h3 className="text">---Make an Appoinment---</h3>
+      </div>
+            
+      <div class="form-row">
+
+      <div class="col-md-6 mb-3">
+      <label for="validationCustom01">Specialization</label>
+      <select class="custom-select" id="validationCustom01" required>
+        <option selected disabled value="">Choose...</option>
+        <option>...</option>
+      </select>
+      <div class="invalid-feedback">Please select one.</div>
+      </div>
+     
+      <div class="col-md-6 mb-3">
+      <label for="validationCustom02" >Doctor Name</label>
+      <select class="custom-select" id="validationCustom02" required>
+        <option selected disabled value="">Choose...</option>
+        <option>...</option>
+      </select>
+      <div class="invalid-feedback">Please select one.</div>
+      </div>
+      </div>
+    
+                  
+      <div class="form-row">
+      <div className="col-md-6 mb-3">
+      <label for="validationDate" >Date</label>
+      <input type="date" class="form-control" id="validationDate" name="date" required/>
+      <div class="invalid-feedback">Please available date.</div>
+      </div>
+       
+
+      <div className="col-md-6 mb-3">
+      <label for="validationCustom03" >Time</label>
+      <input type="time" class="form-control" id="validationCustom03" name="appt" required/>
+      <div class="invalid-feedback">Please available time.</div>
+      </div>
+        
+
+      <div class="col-md-6 mb-3">
+      <label for="validationCustom04">Charges</label>
+      <input type="text" class="form-control" id="validationCustom04" placeholder="Rs.xxxx" required/>
+      <div class="invalid-feedback">Please enter relevant charges</div>
+      </div>
+      </div>
+
+      
+      <div class="form-row">
+      <div class="col-md-6 mb-3">
+      <label for="validationCustom05">First name</label>
+      <input type="text" class="form-control" id="validationCustom05" placeholder="first name" required/>
+      <div class="invalid-feedback">Please enter first name</div>
+      </div>
+
+      <div class="col-md-6 mb-3">
+      <label for="validationCustom06">Last name</label>
+      <input type="text" class="form-control" id="validationCustom06" placeholder="last name"  required/>
+      <div class="invalid-feedback">Please enter last name</div>
+      </div>
+      </div>
+
+
+      <div class="form-row">
+      <div class="col-md-6 mb-3">
+      <label for="validationCustom07">Age</label>
+      <input type="text" class="form-control" id="validationCustom07" placeholder="age" required/>
+      <div class="invalid-feedback">Please enter age</div>
+      </div>
+      </div>
+
+
+      <div class="form-row">
+      < div class="col-md-6 mb-3">
+      <label for="validationCustom08">Mobile Number</label>
+      <input type="text" class="form-control" id="validationCustom08" placeholder="+94xxxxxxxxx" required/>
+      <div class="invalid-feedback">Please enter working number</div>
+      </div>
+      </div>
+      
+
+      <div className="row"></div>
+      <div className="form-group row">
+      <div className="col-sm-10">
+      <div class="mb-3">
+      <button class="signup btn btn-primary" type="submit">continue</button>
+      <div class="row align-items-start">
+
+          
+      </div>
+      </div>
+      </div>
+      </div>
+          
+      
+      </form>
+      </div>
+
+      <div class="doc_footer">
+      <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+      <AfterloginFutter />
+      </div>
+
+      </div> 
+      </div> 
+      
+      </>
+        
+    );
+
+}
+
+export default OnlinePatientAdd
