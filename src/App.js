cc6d6f7a42f05ddfdf8b83951919c641277e75f9
Ireshani99 <ireshanikollure99@gmail.com>
2022-02-12 10:01:11 +0000

    patient add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
+import PhysicalPatientAdd from "./components/PhysicalPatientAdd";
+import OnlinePatientAdd from "./components/OnlinePatientAdd";
+import ReciCard from "./components/ReciCard";
 
-function App() {
+
+const App=()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Router>
+     <Routes> 
+       <Route exact path="/physicalpatientAdd"  element={<PhysicalPatientAdd/>}/>
+       <Route exact path="/onlinePatientAdd"  element={<OnlinePatientAdd/>}/>
+       <Route exact path="/receptionistProfile"  element={<ReciCard/>}/>  
+       
+           
+     </Routes>
+   </Router>
   );
 }
 
 export default App;
+
+
