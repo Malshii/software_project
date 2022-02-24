484f04ff844721c0d5bac61aeae858189e307952
Ireshani99 <ireshanikollure99@gmail.com>
2022-02-24 11:47:55 +0000

    upload lab report

---
+++ src/components/Lab.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import '../components/OnlinePatientAdd.css';
+import NavBar from "./navigationbar";
+import AfterloginFutter from "./AfterloginFutter";
+
+
+function OnlinePatientAdd(){
+
+    return(
+      <>
+      
+      <NavBar/>
+      
+
+     
+
+      <div class="doc_footer">
+      <AfterloginFutter />
+      </div>
+
+      
+      
+      </>
+        
+    );
+
+}
+
+export default OnlinePatientAdd
