49c123987c21046bc1e60d3dcbdf7e57e5578dac
Malshi <kulasinghe1malshi@gmail.com>
2022-06-22 11:01:26 +0530

    changed user controllers

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -8,32 +8,8 @@ const {
 const { genSaltSync, hashSync } = require('bcrypt');
 
 module.exports = {
-  // RegisterUser: async (req, res) => {
-  //   const schema = Joi.object({      
-  //     firstName: Joi.string().min(5).max(50).required(),
-  //     lastName: Joi.string().min(5).max(50).required(),
-  //     email: Joi.string().email().required(),
-  //     phoneNumber: Joi.number().required(),
-  //     dob: Joi.date().raw().required(),
-  //     password: Joi.string().min(5).required(),
-  //     confirmPassword: Joi.string().required().valid(Joi.ref('password')),
-  //   });
-  //   const validation = schema.validate(req.body);
-  //   if (validation.error) {
-  //     res.status(400).send({ message: validation.error.message });
-  //     return;
-  //   }
-  //   const data = validation.value;
-  //   try {
-  //     const result = await signupUser(data);
-  //     result.password = undefined;
-  //     res.status(200).send({ success: result });
-  //   } catch (error) {
-  //     res.status(error.code || 409).send({ message: error.message });
-  //   }
-  // },
   RegisterUser: async (req,res) => {
-    const schema = Joi.object({  
+    const schema = Joi.object({ 
       role : Joi.string().required(),
       firstName: Joi.string().min(5).max(50).required(),
       lastName: Joi.string().min(5).max(50).required(),
@@ -57,19 +33,7 @@ module.exports = {
       res.status(error.code || 409).send({message: error.message});
     }
   },
-  userLogin: async (req, res) => {
-    // const schema = Joi.object({
-    //   email: Joi.string().email().required(),
-    //   password: Joi.string().min(5).required(),
-    //   type: Joi.string().allow(''),
-    // });
-    // console.log(req.body);
-    // const validation = schema.validate(req.body);
-    // if (validation.error) {
-    //   res.status(400).send({ message: validation.error.message });
-    //   return;
-    // }
-    // const body = validation.value;
+  userLogin: async (req, res) => { 
 
     const result = await loginUser(req.body);
     if (result.success) {
--- routes/doctor.profile.route.js
+++ routes/doctor.profile.route.js
@@ -4,23 +4,23 @@ const doctorProfileController = require("../controllers/doctor.profile.controlle
 const doctorProfileService = require("../services/doctor.profile.service")
 
 //Add Doctor profile 
-//localhost:4000/profile/addProfile
+//localhost:4000/doctor/addProfile
 router.post('/addProfile', doctorProfileController.AddDoctorProile);
 
 //Get All Doctor profile 
-//localhost:4000/profile/displaydoctors
+//localhost:4000/doctor/displaydoctors
 router.get('/displaydoctors', doctorProfileService.findAllDoctorProfile);
 
 //Get One Doctor profile 
-//localhost:4000/profile/doctorprofile/:id
+//localhost:4000/doctor/doctorprofile/:id
 router.get('/doctorprofile/:id', doctorProfileService.findOneDoctorProfile);
 
 //Update one Doctor profile 
-//localhost:4000/profile/doctorprofile/:id
+//localhost:4000/doctor/doctorprofile/:id
 router.put('/doctorprofile/:id', doctorProfileService.UpdateOneDoctorProfile);
 
 //Delete one Doctor profile 
-//localhost:4000/profile/doctorprofile/:id
+//localhost:4000/doctor/doctorprofile/:id
 router.delete('/doctorprofile/:id', doctorProfileService.DeleteOneDoctorProfile);
 
 module.exports = router;
